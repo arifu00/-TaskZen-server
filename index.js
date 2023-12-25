f6de040c6c32e55166ac7615873609e1800f3a0b
arifu00 <arifu5758@gmail.com>
2023-12-25 20:37:55 +0600

    implement data post & get method

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-
 app.use(express.json());
-app.use(cookieParser());
-
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kalsdro.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
@@ -25,22 +27,56 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    // task api
+    const taskCollection = client.db("TaskZenDB").collection("tasks");
 
-    const taskCollection = client.db("TaskZenDB").collection("tasks")
+    app.get("/tasks", async (req, res) => {
+      try {
+        let query = {};
+        if (req.query.email) {
+          query.email = req.query.email;
+        }
+        const result = await taskCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    app.get("/tasks/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await taskCollection.findOne(query);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
+    app.post("/tasks", async (req, res) => {
+      try {
+        const data = req.body;
+        const result = await taskCollection.insertOne(data);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
-    
+   
 
     // Connect the client to the server	(optional starting in v4.7)
-   
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -48,11 +84,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Task Zen server running!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
